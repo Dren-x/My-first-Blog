71a0f1e4f5133702a08b8af58b357b21db00db57
Andrey <Dren-x@yandex.ru>
2015-11-14 23:36:26 +0300

    add form message - Step 2 Final + create post - step 3

--- src/Blog/BlogBundle/Controller/PageController.php
+++ src/Blog/BlogBundle/Controller/PageController.php
@@ -2,6 +2,9 @@
 namespace Blog\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Blog\BlogBundle\Entity\Enquiry;
+use Blog\BlogBundle\Form\EnquiryType;
+
 
 class PageController extends Controller
 {
@@ -9,8 +12,33 @@ class PageController extends Controller
     {
         return $this->render('BlogBlogBundle:Page:index.html.twig');
     }
+
     public function contactAction()
     {
-        return $this->render('BlogBlogBundle:Page:contact.html.twig');
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $form->bind($request);
+
+
+            if ($form->isValid()) {
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from symblog')
+                    ->setFrom('enquiries@symblog.co.uk')
+                    ->setTo($this->container->getParameter('blog_blog.emails.contact_email'))
+                    ->setBody($this->renderView('BlogBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+                return $this->redirect($this->generateUrl('BlogBlogBundle_contact'));
+            }
+        }
+
+        return $this->render('BlogBlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+
     }
 }
--- src/Blog/BlogBundle/Form/EnquiryType.php
+++ src/Blog/BlogBundle/Form/EnquiryType.php
@@ -7,15 +7,15 @@ use Symfony\Component\Form\FormBuilderInterface;
 class EnquiryType extends AbstractType
 {
 public function buildForm(FormBuilderInterface $builder, array $options)
-{
-$builder->add('name');
-$builder->add('email', 'email');
-$builder->add('subject');
-$builder->add('body', 'textarea');
-}
+    {
+    $builder->add('name');
+    $builder->add('email', 'email');
+    $builder->add('subject');
+    $builder->add('body', 'textarea');
+    }
 
-public function getName()
-{
-return 'contact';
-}
+    public function getName()
+    {
+    return 'contact';
+    }
 }
